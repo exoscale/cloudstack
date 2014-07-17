adc050eb8dcb2d1e57cc30273544365b2175c9d2
Likitha Shetty <likitha.shetty@citrix.com>
2014-07-17 13:04:58 +0530

    CLOUDSTACK-7119. [VMware] Don't allow VM reset when VM has snapshots.

--- server/test/com/cloud/vm/UserVmManagerTest.java
+++ server/test/com/cloud/vm/UserVmManagerTest.java
@@ -27,6 +27,7 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -86,6 +87,8 @@ import com.cloud.utils.db.EntityManager;
 import com.cloud.utils.exception.CloudRuntimeException;
 import com.cloud.vm.dao.UserVmDao;
 import com.cloud.vm.dao.VMInstanceDao;
+import com.cloud.vm.snapshot.VMSnapshotVO;
+import com.cloud.vm.snapshot.dao.VMSnapshotDao;
 
 public class UserVmManagerTest {
 
@@ -151,6 +154,8 @@ public class UserVmManagerTest {
     UsageEventDao _usageEventDao;
     @Mock
     ServiceOfferingService _serviceOfferingService;
+    @Mock
+    VMSnapshotDao _vmSnapshotDao;
 
     @Before
     public void setup() {
@@ -175,6 +180,7 @@ public class UserVmManagerTest {
         _userVmMgr._entityMgr = _entityMgr;
         _userVmMgr._storagePoolDao = _storagePoolDao;
         _userVmMgr.serviceOfferingService = _serviceOfferingService;
+        _userVmMgr._vmSnapshotDao = _vmSnapshotDao;
 
         doReturn(3L).when(_account).getId();
         doReturn(8L).when(_vmMock).getAccountId();
@@ -186,6 +192,10 @@ public class UserVmManagerTest {
         when(_vmInstance.getServiceOfferingId()).thenReturn(2L);
         when(_vmMock.getServiceOfferingId()).thenReturn(4L);
         when(_offeringDao.findById(4L)).thenReturn(_offeringVo);
+        List<VMSnapshotVO> mockList = mock(List.class);
+        when(_vmSnapshotDao.findByVm(anyLong())).thenReturn(mockList);
+        when(mockList.size()).thenReturn(0);
+
     }
 
     // Test restoreVm when VM state not in running/stopped case
@@ -302,7 +312,9 @@ public class UserVmManagerTest {
         doNothing().when(_volsDao).attachVolume(anyLong(), anyLong(), anyLong());
         when(_volumeMock.getId()).thenReturn(3L);
         doNothing().when(_volsDao).detachVolume(anyLong());
-
+        List<VMSnapshotVO> mockList = mock(List.class);
+        when(_vmSnapshotDao.findByVm(anyLong())).thenReturn(mockList);
+        when(mockList.size()).thenReturn(0);
         when(_templateMock.getUuid()).thenReturn("b1a3626e-72e0-4697-8c7c-a110940cc55d");
 
         Account account = new AccountVO("testaccount", 1L, "networkdomain", (short)0, "uuid");
@@ -347,7 +359,9 @@ public class UserVmManagerTest {
         doNothing().when(_volsDao).attachVolume(anyLong(), anyLong(), anyLong());
         when(_volumeMock.getId()).thenReturn(3L);
         doNothing().when(_volsDao).detachVolume(anyLong());
-
+        List<VMSnapshotVO> mockList = mock(List.class);
+        when(_vmSnapshotDao.findByVm(anyLong())).thenReturn(mockList);
+        when(mockList.size()).thenReturn(0);
         when(_templateMock.getUuid()).thenReturn("b1a3626e-72e0-4697-8c7c-a110940cc55d");
 
         Account account = new AccountVO("testaccount", 1L, "networkdomain", (short)0, "uuid");
