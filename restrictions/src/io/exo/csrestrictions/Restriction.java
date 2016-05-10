91b7785c56fc6f78f81126bd8a01f71d3331c2a4
Marc-Aurèle Brothier <m@brothier.org>
2016-05-10 09:51:50 +0200

    restrictions: importing restrictions project into cloudstack directly, with minimal changes for successful build

---
+++ restrictions/src/io/exo/csrestrictions/Restriction.java
@@ -0,0 +1,42 @@
+package io.exo.csrestrictions;
+
+import java.util.regex.Pattern;
+
+public class Restriction {
+    private Long _maxTemplateSize = null;
+    private String _templateName = null;
+    private Pattern _templateNamePattern = null;
+    private String _serviceOfferingName = null;
+
+    public Long getMaxTemplateSize() {
+        return _maxTemplateSize;
+    }
+
+    public void setMaxTemplateSize(Long maxTemplateSize) {
+        _maxTemplateSize = maxTemplateSize;
+    }
+
+    public String getTemplateName() {
+        return _templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        _templateName = templateName;
+    }
+
+    public String getServiceOfferingName() {
+        return _serviceOfferingName;
+    }
+
+    public void setServiceOfferingName(String serviceOfferingName) {
+        _serviceOfferingName = serviceOfferingName;
+    }
+
+    public Pattern getTemplateNamePattern() {
+        if (_templateName != null && _templateNamePattern == null) {
+            _templateNamePattern = Pattern.compile(_templateName);
+        }
+        return _templateNamePattern;
+    }
+
+}
