ace397c78d143fe6d47cd8907b69bc68c9e4b98c
Marc-Aurèle Brothier <m@brothier.org>
2016-09-22 17:15:38 +0200

    Use full path for jura

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -707,7 +707,7 @@ public class LibvirtComputingResource extends ServerResourceBase implements Serv
             throw new ConfigurationException("Unable to find the security_group.py");
         }
 
-        _juraPath = "jura";
+        _juraPath = "/usr/bin/jura";
 
         _juraState = JuraState.fromString((String) params.get("jura"));
 
