36b1d0f57d302f0823b9bb3d3a33a5b2697ece0f
Marc-Aurèle Brothier <m@brothier.org>
2016-11-15 15:54:32 +0100

    Adds support for four position versions and optional db upgrades
    Often, patch and security releases do not require schema migrations or
    data migrations.  However, if an empty upgrade class and associated
    scripts are not defined, the upgrade process will break.  With this
    change, if a release does not have an upgrade, a noop DbUpgrade is added
    to the upgrade path.  This approach allows the upgrade to proceed and
    for the database to properly reflect the installed version.  This change
    should make the release process simpler as RMs no longer need to
    rememeber to create this boilerplate code when starting a new release.
    
    Beginning with the 4.8.2.0 and 4.9.1.0 releases, the project will
    formally adopt a four (4) position release number to properly accomodate
    rekeases that contain only CVE fixes.  The DatabaseUpgradeChecker and
    Version classes made assumptions that they would always parse and
    compare three (3) position version numbers.  This change adds the
    CloudStackVersion value object that supports both three (3) and four (4)
    version numbers.   It encapsulates version comparsion logic, as well as,
    the rules to allow three (3) and four (4) to interoperate.
    
      * Modifies DatabaseUpgradeChecker to handle derive an upgrade path for
      a version that was not explicitly specified.  It determines the
      releases the first release before it with database migrations and uses
      that list as the basis for the list for version being calculated.  A
      noop upgrade is then added to the list which causes no schema changes
      or data migrations, but will update the database to the version.
      * Adds unit tests for the upgrade path calculation logic in
      DatabaseUpgradeChecker
      * Removes dummy upgrade logic for the 4.8.2.0 introduced in previous
      versions of this patch
      * Introduces the CloudStackVersion value object which parses and
      compares three (3) and four (4) position version numbers.  This class
      is intended to replace com.cloud.maint.Version.
      * Adds the junit-dataprovider dependency -- allowing test data to be
      concisely generated separately from the execution of a test case.
      Used extensively in the CloudStackVersionTest.
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    Cherry-pick from commit 8d11511b1f2ac20b51c8f3f5290139a19a654672

---
+++ engine/schema/test/com/cloud/upgrade/DatabaseUpgradeCheckerTest.java
@@ -0,0 +1,147 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package com.cloud.upgrade;
+
+import com.cloud.upgrade.dao.DbUpgrade;
+import com.cloud.upgrade.dao.Upgrade410to420;
+import com.cloud.upgrade.dao.Upgrade420to421;
+import com.cloud.upgrade.dao.Upgrade421to430;
+import com.cloud.upgrade.dao.Upgrade430to440;
+import com.cloud.upgrade.dao.Upgrade440to441;
+import com.cloud.upgrade.dao.Upgrade441to442;
+import com.cloud.utils.CloudStackVersion;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class DatabaseUpgradeCheckerTest {
+
+    @Test
+    public void testCalculateUpgradePath440to441() {
+
+        final CloudStackVersion dbVersion = CloudStackVersion.parse("4.4.0");
+        assertNotNull(dbVersion);
+
+        final CloudStackVersion currentVersion = CloudStackVersion.parse("4.4.1");
+        assertNotNull(currentVersion);
+
+        final DatabaseUpgradeChecker checker = new DatabaseUpgradeChecker();
+        final DbUpgrade[] upgrades = checker.calculateUpgradePath(dbVersion, currentVersion);
+
+        assertNotNull(upgrades);
+        assertEquals(3, upgrades.length);
+        assertTrue(upgrades[0] instanceof Upgrade440to441);
+        assertTrue(upgrades[1] instanceof Upgrade441to442);
+    }
+
+    @Test
+    public void testCalculateUpgradePath440to4420() {
+
+        final CloudStackVersion dbVersion = CloudStackVersion.parse("4.4.0");
+        assertNotNull(dbVersion);
+
+        final CloudStackVersion currentVersion = CloudStackVersion.parse("4.4.2");
+        assertNotNull(currentVersion);
+
+        final DatabaseUpgradeChecker checker = new DatabaseUpgradeChecker();
+        final DbUpgrade[] upgrades = checker.calculateUpgradePath(dbVersion, currentVersion);
+
+        assertNotNull(upgrades);
+        assertEquals(2, upgrades.length);
+
+        assertTrue(upgrades[0] instanceof Upgrade440to441);
+
+        assertTrue(Arrays.equals(new String[] { "4.4.1", currentVersion.toString()}, upgrades[1].getUpgradableVersionRange()));
+        assertEquals(currentVersion.toString(), upgrades[1].getUpgradedVersion());
+
+    }
+
+    @Test
+    public void testCalculateUpgradePath441to4420() {
+
+        final CloudStackVersion dbVersion = CloudStackVersion.parse("4.4.1");
+        assertNotNull(dbVersion);
+
+        final CloudStackVersion currentVersion = CloudStackVersion.parse("4.4.2");
+        assertNotNull(currentVersion);
+
+        final DatabaseUpgradeChecker checker = new DatabaseUpgradeChecker();
+        final DbUpgrade[] upgrades = checker.calculateUpgradePath(dbVersion, currentVersion);
+
+        assertNotNull(upgrades);
+        assertEquals(1, upgrades.length);
+
+        assertTrue(Arrays.equals(new String[] { "4.4.1", currentVersion.toString()}, upgrades[0].getUpgradableVersionRange()));
+        assertEquals(currentVersion.toString(), upgrades[0].getUpgradedVersion());
+
+    }
+
+    @Test
+    public void testFindUpgradePath430to441() {
+
+        final CloudStackVersion dbVersion = CloudStackVersion.parse("4.3.0");
+        assertNotNull(dbVersion);
+
+        final CloudStackVersion currentVersion = CloudStackVersion.parse("4.4.1");
+        assertNotNull(currentVersion);
+
+        final DatabaseUpgradeChecker checker = new DatabaseUpgradeChecker();
+        final DbUpgrade[] upgrades = checker.calculateUpgradePath(dbVersion, currentVersion);
+
+        assertNotNull(upgrades);
+        assertEquals(4, upgrades.length);
+
+        assertTrue(upgrades[0] instanceof Upgrade430to440);
+        assertTrue(upgrades[1] instanceof Upgrade440to441);
+        assertTrue(upgrades[2] instanceof Upgrade441to442);
+
+    }
+
+    @Test
+    public void testFindUpgradePath410to4420() {
+
+        final CloudStackVersion dbVersion = CloudStackVersion.parse("4.1.0");
+        assertNotNull(dbVersion);
+
+        final CloudStackVersion currentVersion = CloudStackVersion.parse("4.4.2");
+        assertNotNull(currentVersion);
+
+        final DatabaseUpgradeChecker checker = new DatabaseUpgradeChecker();
+        final DbUpgrade[] upgrades = checker.calculateUpgradePath(dbVersion, currentVersion);
+
+        assertNotNull(upgrades);
+        assertEquals(6, upgrades.length);
+
+        assertTrue(upgrades[0] instanceof Upgrade410to420);
+        assertTrue(upgrades[1] instanceof Upgrade420to421);
+        assertTrue(upgrades[2] instanceof Upgrade421to430);
+        assertTrue(upgrades[3] instanceof Upgrade430to440);
+        assertTrue(upgrades[4] instanceof Upgrade440to441);
+        assertTrue(upgrades[5] instanceof Upgrade441to442);
+
+        assertTrue(Arrays.equals(new String[] { "4.4.1", currentVersion.toString()}, upgrades[5].getUpgradableVersionRange()));
+        assertEquals(currentVersion.toString(), upgrades[5].getUpgradedVersion());
+
+    }
+}
